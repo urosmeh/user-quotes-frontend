7b668b4e04aada1dc0924ea76a0ffa905b67e395
Uros Meh <uros.meh@pro-bit.si>
2023-01-08 19:51:16 +0100

    added react-router-dom package + LandingPage component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
+import { Route, Routes } from "react-router-dom";
 import { Footer } from "./components/layouts/footer/Footer";
 import { Navbar } from "./components/layouts/navbar/Navbar";
 import "./index.css";
 import classes from "./App.module.css";
+import { LandingPage } from "./components/pages/LandingPage";
 
 function App() {
   return (
     <div className={classes.app}>
       <Navbar />
-      <div className={classes.content}>Content</div>
+      <div className={classes.content}>
+        <Routes>
+          <Route path="/" element={<LandingPage />}></Route>
+          <Route path="/login">
+            <>Test</>
+          </Route>
+        </Routes>
+      </div>
       <Footer />
     </div>
   );
